19dc036573b7abba044b95ea65b06a0bc1e5adbf
Arisa Moonsammy <aricollegem@gmail.com>
2025-07-08 08:52:14 -0400

    commit

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let currentSlide = 0;
 const slides = document.querySelectorAll('.slide');
 const totalSlides = slides.length;
+const items = document.querySelectorAll(".timeline-item");
 
 function showSlide(index) {
     slides.forEach((slide, i) => {
@@ -17,3 +18,35 @@ if (slides.length > 0) {
     showSlide(currentSlide)
     setInterval(nextSlide, 5000);
 }
+
+function showItemsOnScroll() {
+    const triggerBottom = window.innerHeight * 0.85;
+    const items = document.querySelectorAll(".timeline-item");
+
+    items.forEach(item => {
+        const itemTop = item.getBoundingClientRect().top;
+        if (itemTop < triggerBottom) {
+            item.classList.add("show");
+        }
+    });
+}
+
+document.querySelectorAll('.company-tab').forEach(tab => {
+    tab.addEventListener('click', function() {
+
+        document.querySelectorAll('.company-tab').forEach(t => t.classList.remove('active'));
+        this.classList.add('active');
+
+        document.querySelectorAll('.timeline-item').forEach(item => {
+            item.style.display = 'none';
+        });
+
+        const company = this.getAttribute('data-company');
+        const selected = document.querySelector(`.timeline-item[data-company="${company}"]`);
+        if (selected) selected.style.display = '';
+    });
+});
+
+window.addEventListener("scroll", showItemsOnScroll);
+window.addEventListener("load", showItemsOnScroll);
+
